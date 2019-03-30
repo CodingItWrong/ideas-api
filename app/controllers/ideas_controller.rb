4aea5e857d2ffb1c4765fcfa477787d8b4bf81f2
Josh Justice <me@codingitwrong.com>
2019-03-30 14:34:57 -0400

    Restrict controllers to logged-in users

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,3 @@
 class IdeasController < ApplicationController
+  before_action :doorkeeper_authorize!
 end
--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,2 +1,3 @@
 class SourcesController < ApplicationController
+  before_action :doorkeeper_authorize!
 end
