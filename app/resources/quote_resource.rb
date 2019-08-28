7c5194dbabba0051747a66de66fe85cd363c9b6c
Josh Justice <josh.justice@accesscfa.com>
2019-08-28 06:59:29 -0400

    Expose quote created_at

--- app/resources/quote_resource.rb
+++ app/resources/quote_resource.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class QuoteResource < ApplicationResource
-  attributes :text, :location, :comments
+  attributes :text, :location, :comments, :created_at
 
   has_one :source
   has_one :idea
